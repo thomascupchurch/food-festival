e9adcb075b3c0e115efd7008313ac13bbd3a3837
Boba Fett <thomascupchurch@gmail.com>
2021-10-12 17:54:46 -0500

    add .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
   entry: "./assets/js/script.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.bundle.js",
   },
-  plugins: [],
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
   mode: "development",
 };
